f1d7b4cce943c613eee11479749ef101d55cbd9a
muhammeds <adekolamuhammed89gmail.com>
2024-07-21 23:04:59 +0100

    fixed modal bug

--- src/components/ListDetails.js
+++ src/components/ListDetails.js
@@ -3,9 +3,9 @@ import '../App.css';
 import { MdDelete } from "react-icons/md";
 import { FaRegFaceSadTear } from "react-icons/fa6";
 
-
 const ListDetails = ({ list , setList }) => {
-    const [modal , setModal] = useState(false)
+    const [modal, setModal] = useState(false);
+    const [itemToDelete, setItemToDelete] = useState(null);
 
     const handleCheckboxChange = (id) => {
         setList((list) =>
@@ -16,54 +16,54 @@ const ListDetails = ({ list , setList }) => {
     };
 
     const handleDelete = (id) => {
-        setList((list) => list.filter(item => item.id !== id))
-        setModal(false)
+        setList((list) => list.filter(item => item.id !== id));
+        setModal(false);
     };
 
-    const CloseModalFromBackground = (e) =>{
-        if(e.target.classList.contains('modalbackground')){
-            setModal(false)
+    const CloseModalFromBackground = (e) => {
+        if (e.target.classList.contains('modalbackground')) {
+            setModal(false);
         }
-    }
+    };
 
     return (
         <div className='listdetails'>
-                    
-        {list.length ? list.map((item) => (
-                <>
-                { modal ? <div className='modalbackground' onClick={CloseModalFromBackground}>
-                <div className='modal'>
-                    <p>Delete Todo List!!</p>
-                    <div>
-                        <button onClick={() => handleDelete(item.id)} >Yes</button>
-                        <button onClick={()=> setModal(false)}>No</button>
+            {list.length ? list.map((item) => (
+                <div key={item.id}>
+                    <div className='eachlist'>
+                        <div>
+                            <input className='checkbox'
+                                type='checkbox'
+                                checked={item.isDone}
+                                onChange={() => handleCheckboxChange(item.id)}
+                            />
+                            <p
+                                style={{ textDecoration: item.isDone ? 'line-through' : 'none', color: item.isDone ? 'grey' : 'black' }}
+                            >
+                                {item.todo}
+                            </p>
+                        </div>
+                        <div>
+                            <MdDelete className='delete' onClick={() => { setModal(true); setItemToDelete(item.id); }}/>
+                        </div>
                     </div>
-                 </div>
-            </div> : null } 
-                <div className='eachlist' key={item.id}>
-                    <div>
-                    <input className='checkbox'
-                        type='checkbox'
-                        checked={item.isDone}
-                        onChange={() => handleCheckboxChange(item.id)}
-                    />
-                    <p
-                        style={{ textDecoration: item.isDone ? 'line-through' : 'none', color: item.isDone ? 'grey' : 'black' }}
-                    >
-                        {item.todo}
-                    </p>
-                    </div>
-                  
-                    <div>
-                        <MdDelete className='delete' onClick={()=> setModal(true)}/>
-                   
-                    </div>
-
+                    <div className='hr'></div>
                 </div>
-                <div className='hr'> </div>
-                </>
+            )) : (
+                <p>No Todos Available<FaRegFaceSadTear className='sad'/>!!</p>
+            )}
 
-            )) : <p>No Todos Available<FaRegFaceSadTear className='sad'/>!!</p>}
+            {modal && (
+                <div className='modalbackground' onClick={CloseModalFromBackground}>
+                    <div className='modal'>
+                        <p>Delete Todo List!!</p>
+                        <div>
+                            <button onClick={() => handleDelete(itemToDelete)}>Yes</button>
+                            <button onClick={() => setModal(false)}>No</button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
